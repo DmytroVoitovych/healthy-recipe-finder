6181d833e6c19ca4988a43326fbd233b8c4673f3
DmytroVoitovych <begunec23@gmail.com>
2025-09-29 21:25:43 +0300

    Refactor RecipesListSection component and add CSS grid styles for recipe display

--- src/components/recipesList/RecipesListSection.tsx
+++ src/components/recipesList/RecipesListSection.tsx
@@ -1,18 +1,30 @@
-import { RecipesResponse } from "@/lib/api/fetchRecipesTypes";
 import { RecipesForm } from "./RecipesForm";
 import { RecipeCard } from "./RecipeCard";
+import { RecipeResponse } from "@/lib/api/fetchRecipesTypes";
+import { FetchRecipesParams } from "@/lib/api/fetchRecipes";
+import styles from "./recipesListSection.module.css";
 
-export const RecipesListSelection = ({recipeList}:{recipeList:RecipesResponse}) => {
+interface RecipesListSectionProps {
+  recipeList: RecipeResponse;
+  params: FetchRecipesParams;
+}
+
+export const RecipesListSelection = ({
+  recipeList,
+  params,
+}: RecipesListSectionProps) => {
+
+
   return (
     <section>
       <h1 className="visually-hidden">
         Healthy Recipe Collection - Quick Mediterranean, Vegetarian and High-Protein
         Meals Under 30 Minutes.
       </h1>
-      <RecipesForm/>
-      <ul>
-        {recipeList.recipes.recipe.map((recipe) => (
-          <RecipeCard key={recipe.recipe_id} recipe={recipe}/>
+      <RecipesForm />
+      < ul className={styles.recipesList}>
+        {recipeList.data.map((recipe) => (
+          <RecipeCard key={recipe.id} recipe={recipe} />
         ))}
       </ul>
     </section>
